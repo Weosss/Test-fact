7cbfffbf7d20e0e54d35268a7175edec125f66fb
Cтепан Канатников <cansteppp@gmail.com>
2020-07-01 18:21:42 +0300

    Добавления кода вычисления факториала числа

---
+++ Test comit/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return n * fact(n - 1);
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Цифру гани, ща факториал считать буим: ";
+	cout << "15 ";
+	cin >> a;
+	cout << fact(a) << endl;
+}
